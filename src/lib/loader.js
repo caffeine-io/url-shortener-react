14f3d8945e24c3737ba7a4482f3f0a391aa9c676
caffeine-io <abidialaa@outlook.com>
2025-02-03 23:55:37 +0100

    add api url

--- src/lib/loader.js
+++ src/lib/loader.js
@@ -1,8 +1,10 @@
+export const apiUrl = import.meta.env.VITE_BASE_URL;
+
 export const createShortUrlAction = async ({ request }) => {
   const formData = await request.formData();
   const url = formData.get("url");
   try {
-    const response = await fetch("/api/urls", {
+    const response = await fetch(`${apiUrl}/api/urls`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ originalUrl: url }),
